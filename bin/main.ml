2d5485e633de76191119bc39b8193f1d786188bc
Christopher Lang <christopher.lang.256@gmail.com>
2025-02-11 19:46:39 +0000

    Convert propositional expression to clausal form

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,10 @@
 open Sat
 
-let () = print_endline ("Hello, World! " ^ string_of_int (fib 10))
+(* let e = Complement ( Disjunction ( Variable 1, Variable 2 ) ) *)
+let e = Complement ( Disjunction ( Conjunction ( Variable 1, Disjunction(Variable 2, Variable 3) ), Disjunction ( Variable 4, Variable 5 ) ) )
+
+let () = print_endline (
+         (string_of_prop_expr string_of_int e)
+       ^ " === "
+       ^ (string_of_cnf_expr string_of_int (prop_to_cnf e))
+       )
--- lib/sat.ml
+++ lib/sat.ml
@@ -1,4 +1,35 @@
-let rec fib = function
-  | 0 -> 0
-  | 1 -> 1
-  | n -> fib (n-1) + fib (n-2)
+type 'a prop_expr =
+  | Variable of 'a
+  | Complement of 'a prop_expr
+  | Conjunction of 'a prop_expr * 'a prop_expr
+  | Disjunction of 'a prop_expr * 'a prop_expr
+
+type 'a literal =
+  | Positive of 'a
+  | Negative of 'a
+
+type 'a cnf_expr = 'a literal list list
+
+
+let string_of_literal f = function
+  | Positive a -> f a
+  | Negative a -> "!" ^ ( f a )
+
+let rec string_of_prop_expr f = function
+  | Variable a -> f a
+  | Complement e -> "!" ^ (string_of_prop_expr f e)
+  | Conjunction (e1,e2) -> "(" ^ (string_of_prop_expr f e1) ^ "*" ^ (string_of_prop_expr f e2) ^ ")"
+  | Disjunction (e1,e2) -> "(" ^ (string_of_prop_expr f e1) ^ "+" ^ (string_of_prop_expr f e2) ^ ")"
+
+let string_of_cnf_expr f cnf = String.concat "^" (
+    List.map (fun c -> "{" ^ ( String.concat ", " (List.map (string_of_literal f) c) ) ^ "}") cnf
+  )
+
+let rec prop_to_cnf = function
+  | Variable a -> [ [ Positive a ] ]
+  | Complement (Variable a) -> [ [ Negative a ] ]
+  | Complement (Complement e) -> prop_to_cnf e
+  | Complement (Conjunction (e1, e2)) -> prop_to_cnf (Disjunction (Complement (e1), Complement(e2)))
+  | Complement (Disjunction (e1, e2)) -> prop_to_cnf (Conjunction (Complement (e1), Complement(e2)))
+  | Conjunction (e1, e2) -> (prop_to_cnf e1) @ (prop_to_cnf e2)
+  | Disjunction (e1, e2) -> List.concat_map (fun c1 -> List.map (fun c2 -> c1 @ c2) (prop_to_cnf e2)) (prop_to_cnf e1)
